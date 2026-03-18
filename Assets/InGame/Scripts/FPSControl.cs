31549ad81403e56a3be4b4dfaa0b5972da567bb5
LilHotPocket-JA <justin.antulio.arroyo@gmail.com>
2026-03-18 16:54:31 -0400

    Ambatublou

--- Assets/InGame/Scripts/FPSControl.cs
+++ Assets/InGame/Scripts/FPSControl.cs
@@ -3,17 +3,28 @@ using UnityEngine;
 public class FPSControl : MonoBehaviour
 {
     private CharacterController chara;
-    public float walkSpd = 5;
+
+
+    [Range(0, 30)] public float walkSpd = 5;
+    [Range(0, 10)] public float sensitivity = 2;
+    [Range(0, 10)]  public float jumpHeight;
+
     float vertR;
     public float lookLimit = 80f;
-    public float sensitivity = 2;
-
     private Camera cam;
     private float defWalk;
+    private float runSpd;
+    private Vector3 currMovement;
+    private float g = 9.81f;
+
+    private Vector3 hitPoint;
+    public ParticleSystem impact;
+    [Range(10,30)] public int particleC = 20;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        runSpd = walkSpd * 2;
         defWalk = walkSpd;
         chara = GetComponent<CharacterController>();
         Cursor.visible = false;
@@ -27,15 +38,26 @@ public class FPSControl : MonoBehaviour
     {
         Movement();
         MouseLook();
+        Jumping();
 
         if (Input.GetKey(KeyCode.LeftShift)) 
         {
-            walkSpd = 10; 
+            walkSpd = runSpd; 
         }
 
         else 
         {
             walkSpd = defWalk;
+        }
+
+        if (ObjectInFocus() != null)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                impact.transform.position = hitPoint;
+                impact.Emit(particleC);
+                DestroyObject(ObjectInFocus().gameObject);
+            }
         }
 
 
@@ -53,8 +75,10 @@ public class FPSControl : MonoBehaviour
         float horSpeed = horInput * walkSpd;
 
         Vector3 horizontalMov = new Vector3(horSpeed, 0, vSpeed);
+        currMovement.x = horizontalMov.x;
+        currMovement.z = horizontalMov.z;
 
-        chara.Move(horizontalMov * Time.deltaTime);
+        chara.Move(currMovement * Time.deltaTime);
     }
 
      void MouseLook()
@@ -65,5 +89,36 @@ public class FPSControl : MonoBehaviour
         vertR -= Input.GetAxis("Mouse Y") * sensitivity;
         vertR = Mathf.Clamp(vertR, -lookLimit , lookLimit);
         cam.transform.localRotation = Quaternion.Euler(vertR, 0, 0);
+    }
+
+    void Jumping()
+    {
+        if (chara.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                currMovement.y = jumpHeight;
+                Debug.Log("jump");
+            }
+            
+        }
+
+        else
+        {
+            currMovement.y -= g * Time.deltaTime;
+            Debug.Log("falling");
+        }
+    }
+
+    public GameObject ObjectInFocus()
+    {
+        GameObject result = null;
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit))
+        {
+            result = hit.transform.gameObject;
+            hitPoint = hit.point;
+        }
+        return result;
     }
 }
